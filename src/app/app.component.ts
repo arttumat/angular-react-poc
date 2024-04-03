9f5cc9df2b0b937debb8299e9e8bd464d4ce90d0
Arttu Matilainen <arttu.matilainen@cgi.com>
2024-04-03 12:30:18 +0300

    add code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactTestPageComponent } from './components/kesko-contentful/react-test.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactTestPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'tuotekortti-poc';
---
+++ src/app/components/kesko-contentful/react-test.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import type { ComponentProps } from 'react';
+import { LazyReactComponentDirective } from '../../directives/react-ng.directive';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, LazyReactComponentDirective],
+  template: `
+    <h1>Todos page</h1>
+    <button (click)="showSelect = true">Show React Component</button>
+    <ng-container *ngIf="showSelect">
+      <button (click)="changeProps()">Change</button>
+      <div [lazyReactComponent]="Select" [props]="selectProps"></div>
+    </ng-container>
+  `,
+  selector: 'app-react-test-page',
+})
+export class ReactTestPageComponent {
+  showSelect = false;
+  selectProps: ComponentProps<typeof import('react-select').default> = {
+    onChange(v) {
+      console.log(v);
+    },
+    options: [
+      { value: 'chocolate', label: 'Chocolate' },
+      { value: 'strawberry', label: 'Strawberry' },
+      { value: 'vanilla', label: 'Vanilla' },
+    ],
+  };
+
+  Select = () => import('react-select').then((m) => m.default);
+
+  changeProps() {
+    this.selectProps = {
+      ...this.selectProps,
+      options: [{ value: 'change', label: 'Change' }],
+    };
+  }
+}
